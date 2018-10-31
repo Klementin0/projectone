6f8612a6b264419af2a431ea0098a84783887813
twillighagen <t.k.willighagen@st.hanze.nl>
2018-10-31 20:24:13 +0100

    Snel Knoppie

---
+++ window.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="knoppie", fg="black")
+
+button1.pack()
+
+root.mainloop()
