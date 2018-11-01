1ea0faad353d5d89a0d84b00d08d1d9cd00aa1a1
twillighagen <t.k.willighagen@st.hanze.nl>
2018-11-01 15:55:48 +0100

    Return 1 on button click

--- window.py
+++ window.py
@@ -2,15 +2,10 @@ from tkinter import *
 
 root = Tk() # Root word main Tk()
 
-topFrame = Frame(root) # topFrame van Root aanmaken
-topFrame.pack() #Plaats topFrame in de window
-bottomFrame = Frame(root) #bottomFrame van root aanmaken
-bottomFrame.pack(side=BOTTOM) #bottomFrame aan de onderkant van de window toevoegen
+def command(d):
+    print(d)
 
-button1 = Button(topFrame, text="knoppie", fg="black") #button1 word een nieuwe button, word geplaats in het topFrame, bevat de text "knoppie" en is van de kleur "Black"
-
-
-button1.pack() #Plaats de button in het window
-
+button1 = Button(root, text="click me", command=lambda : command("1"))
+button1.pack()
 
 root.mainloop() #Zorg er voor dat de window niet in een keer weggaat door er een loop van te maken.
