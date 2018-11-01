8260aa23baa368a65667d6b1cc7dfc7329dd3ab9
twillighagen <t.k.willighagen@st.hanze.nl>
2018-11-01 13:04:05 +0100

    Added Comment

--- window.py
+++ window.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
-root = Tk()
+root = Tk() # Root word main Tk()
 
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+topFrame = Frame(root) # topFrame van Root aanmaken
+topFrame.pack() #Plaats topFrame in de window
+bottomFrame = Frame(root) #bottomFrame van root aanmaken
+bottomFrame.pack(side=BOTTOM) #bottomFrame aan de onderkant van de window toevoegen
 
-button1 = Button(topFrame, text="knoppie", fg="black")
-
-
-button1.pack()
+button1 = Button(topFrame, text="knoppie", fg="black") #button1 word een nieuwe button, word geplaats in het topFrame, bevat de text "knoppie" en is van de kleur "Black"
 
 
-root.mainloop()
+button1.pack() #Plaats de button in het window
+
+
+root.mainloop() #Zorg er voor dat de window niet in een keer weggaat door er een loop van te maken.
