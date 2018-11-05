7392c91963295abd2f95f74527fc09fb3ed9eeca
twillighagen <t.k.willighagen@st.hanze.nl>
2018-11-05 14:37:54 +0100

    Added Serial RX and TX

--- window.py
+++ window.py
@@ -8,4 +8,4 @@ def command(d):
 button1 = Button(root, text="click me", command=lambda : command("1"))
 button1.pack()
 
-root.mainloop() #Zorg er voor dat de window niet in een keer weggaat door er een loop van te maken.
+root.mainloop() # Zorg er voor dat de window niet in een keer weggaat door er een loop van te maken.
