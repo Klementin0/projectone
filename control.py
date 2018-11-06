58f2628282a0a6d725f733ab01e62228f934c125
twillighagen <t.k.willighagen@st.hanze.nl>
2018-11-06 14:50:17 +0100

    Naam veranderd van contol -> control

---
+++ control.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+import serial_se_re
+serialPort = serial_se_re.SerialPort()
+
+def SendDataCommand():
+    serialPort.Stuur("KEK")
+
+def Verbinden():
+    serialPort.Open("COM5",19200)
+
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+
+connect_button = QPushButton('Connect')
+layout.addWidget(connect_button)
+connect_button.clicked.connect(lambda: Verbinden())
+
+stuur_data_button = QPushButton('Stuur message')
+layout.addWidget(stuur_data_button)
+stuur_data_button.clicked.connect(lambda: SendDataCommand())
+
+window.setLayout(layout)
+window.show()
+app.exec_()
