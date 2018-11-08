146b32a98e8b5df7a3c312ec5f7a870939251599
Youri van de Geer <yourivandegeer123@gmail.com>
2018-11-08 14:08:47 +0100

    ProjectSensoren

---
+++ sensormodule/SensorProject/SensorProject/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Afstandssensor.c
+
+AVR_TTC_scheduler.c
+
+ldr.c
+
+SensorProject.c
+
