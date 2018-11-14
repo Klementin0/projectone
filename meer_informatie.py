48044477a7e63cc6cb8a5ae310dadd31565ad4f5
t.de.pater@st.hanze.nl <t.de.pater@st.hanze.nl>
2018-11-14 13:30:32 +0100

    __pycache__ verwijderd

--- meer_informatie.py
+++ meer_informatie.py
@@ -1,23 +1,34 @@
-import plotly.graph_objs as go
+from plotly import tools
 import plotly.offline as ply
-#from control import Return_lis
+import plotly.graph_objs as go
 
+temp = [20,30,20,10,29]
 
-
-temp = [20,21,20,19,20,21,23,55,23,65]
-
-
-
-#X-ass maken met intervallen van 3
-numbers = []
+xass = []
 for i in range(len(temp)):
-   numbers.append(i * 3)
+    xass.append(i * 3)
 
-# Create a trace
-trace = go.Scatter(
-    x = numbers,
-    y = temp
-)
-data = [trace]
+temperatuur = go.Scatter(
+    x= xass,
+    y= temp)
 
-ply.plot(data, filename='grafiek.html')
+licht = go.Scatter(
+    x= xass,
+    y=[50, 60, 70])
+
+afstand = go.Scatter(
+    x= xass,
+    y=[600, 700, 800])
+
+
+fig = tools.make_subplots(rows=3, cols=1, subplot_titles=('Temperatuur', 'licht',
+                                                          'afstand'))
+
+fig.append_trace(temperatuur, 1, 1)
+fig.append_trace(licht, 2, 1)
+fig.append_trace(afstand, 3, 1)
+
+
+fig['layout'].update(height=600, width=600, title='multi.html')
+
+ply.plot(fig, filename='multi.html')
